10c1b07e06857ca82ca81d2e29f518cf32b502b2
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-06 20:56:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,10 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
 # start
 
 def won?(board)
@@ -53,9 +57,7 @@ end
 
 
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
+
 
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
