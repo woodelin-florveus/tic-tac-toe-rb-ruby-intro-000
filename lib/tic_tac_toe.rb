56af548273e5628cb9d0db2f3bbbbd747b4f7d77
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-06 21:09:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+   if index.between?(0,8) && !position_taken?(board, index)
+      return true
+   end
 end
 
 def turn(board)
