dfed9c47022657322a8ccc74dcb4018832b3b5f9
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-06 20:46:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,3 +40,20 @@ def winner(board)
     board[winning_combo.first]
   end
 end
+
+def turn_count(board)
+  # board.count{|token| token == "X" || token == "O"}
+
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
