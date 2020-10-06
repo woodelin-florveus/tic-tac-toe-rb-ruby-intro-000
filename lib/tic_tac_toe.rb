d6d6d2455facb5ef2943f5065f53da4f61d687c9
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-06 20:57:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,6 +29,9 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
 # start
 
@@ -65,9 +68,7 @@ end
 
 
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
+
 
 def turn(board)
   puts "Please enter 1-9:"
