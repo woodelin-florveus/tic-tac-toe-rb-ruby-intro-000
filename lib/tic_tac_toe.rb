8385282f042d7d5db9152b845a49fa8a82e86243
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-06 21:04:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,4 +89,13 @@ def winner(board)
   end
 end
 
-# end
+def play(board)
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
